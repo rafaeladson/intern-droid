11c5b2c325eb190268c57f876782fc033d760b7c
rafael.adson <rafael@fiive.net>
2012-05-16 23:11:36 -0300

    Edit text content length validation.
    Created a method to validate that the text in a EditText has at most n characters. If the text is longer than a validation message will be appended to the editText.

--- android-test/src/net/fiive/intern/android/test/view/TextValidatorTest.java
+++ android-test/src/net/fiive/intern/android/test/view/TextValidatorTest.java
@@ -41,6 +41,52 @@ public class TextValidatorTest extends AndroidTestCase {
 		boolean validated = validator.validateTextNotEmpty(editText, "Null Text");
 		assertFalse(validated);
 	}
+
+	public void testTextMaxLength_zeroLength() {
+		editText.setText("");
+
+		boolean validated = validator.validateTextMaxLength(editText, 1, "Null Text");
+		assertTrue(validated);
+		assertNull(editText.getError());
+	}
+
+	public void testTextMaxLength_maxLength() {
+		editText.setText("10 Letters");
+		boolean validated = validator.validateTextMaxLength(editText, 10, "Should not appear");
+		assertTrue(validated);
+		assertNull(editText.getError());
+	}
+
+	public void testTextMaxLength_maxLengthPlusOne() {
+		editText.setText("9 Letters");
+		String message = "Text should have at most 8 letters";
+		boolean validated = validator.validateTextMaxLength(editText, 8, message);
+		assertFalse(validated);
+		assertEquals(message, editText.getError().toString());
+	}
+
+	public void testMaxLength_maxLengthEqualsZero() {
+		editText.setText("");
+		assertTrue(validator.validateTextMaxLength(editText, 0, "null message"));
+		editText.setText("a");
+		assertFalse(validator.validateTextMaxLength(editText, 0, "null message"));
+	}
+
+	public void testMaxLength_errorChangeToNotNullWhenInvalidThenChangesBackToNullWhenValid() {
+		editText.setText("hello");
+		String message = "foo";
+		assertTrue(validator.validateTextMaxLength(editText, 6, message));
+		assertNull(editText.getError());
+
+		assertFalse(validator.validateTextMaxLength(editText, 4, message));
+		assertEquals(message, editText.getError().toString());
+
+		assertTrue(validator.validateTextMaxLength(editText, 5, message));
+		assertNull(editText.getError());
+	}
+
+
+
 }
 
 
